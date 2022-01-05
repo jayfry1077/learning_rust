2252d490a98d2beb9f85ad13a4245daf7b519478
Jonathan Bradbury <53982942+jayfry1077@users.noreply.github.com>
2022-01-04 21:46:14 -0700

    chore: ...

--- chapter_2/guessing_game/target/rls/debug/deps/guessing_game-4e0c718314a6eb25.d
+++ chapter_2/guessing_game/target/rls/debug/deps/guessing_game-4e0c718314a6eb25.d
@@ -1,5 +1,5 @@
-c:\Users\jonat\Documents\GitHub\learning_rust\guessing_game\target\rls\debug\deps\guessing_game-4e0c718314a6eb25.rmeta: src\main.rs
+c:\Users\jonat\Documents\GitHub\learning_rust\chapter_2\guessing_game\target\rls\debug\deps\guessing_game-4e0c718314a6eb25.rmeta: src\main.rs
 
-c:\Users\jonat\Documents\GitHub\learning_rust\guessing_game\target\rls\debug\deps\guessing_game-4e0c718314a6eb25.d: src\main.rs
+c:\Users\jonat\Documents\GitHub\learning_rust\chapter_2\guessing_game\target\rls\debug\deps\guessing_game-4e0c718314a6eb25.d: src\main.rs
 
 src\main.rs:
